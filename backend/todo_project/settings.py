4d06bce94355aa23dceaa66da268aeaf0db2e518
manikshivam <manikshivam708@gmail.com>
2026-05-08 22:43:24 +0530

    login validation

--- backend/todo_project/settings.py
+++ backend/todo_project/settings.py
@@ -46,8 +46,12 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ]
 }
 
